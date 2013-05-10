f2e9695288330a6bfad24531e19dffcacb110104
mlorenz92 <mlorenz1992@gmail.com>
2013-05-09 23:11:06 -0400

    removed unecessary blocked out code and updated it to work with web.py

--- server.py
+++ server.py
@@ -1,12 +1,32 @@
-#import web
-import json
+#!/usr/bin/env python
+import web
+#import json
+#import re
 from script import redditParser
 
+render = web.template.render('')
 
+urls = (
+	'/', 'index'
+	)
 
-class info:
+class index:
 
 	def GET(self):
-		redditLinks= redditParser()
 
-info().GET()
+		redditLinks= redditParser().main()  
+
+		return render.index(redditLinks)
+
+class links:
+
+	def GET(self):
+		
+		redditLinks= redditParser().main()
+
+
+#info().GET()
+
+if __name__ == "__main__": 
+    app = web.application(urls, globals())
+    app.run()   
