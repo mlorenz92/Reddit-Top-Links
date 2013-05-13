5098515454adb4ff28e4c2fdc5abe40960b6f63f
mlorenz92 <mlorenz1992@gmail.com>
2013-05-13 11:24:21 -0400

    adding documentation and comments to explain key components

--- script.py
+++ script.py
@@ -3,18 +3,21 @@ import praw
 import json
 
 
+
 class redditParser:
 
 	def main(self):
 		r = praw.Reddit(user_agent='testing out Reddit API through PRAW by /u/ethnographythroway')
 		submissions = r.get_front_page(limit=1)
+		#You can log in below by simply typing in your ('username','password') and the wrapper will automatically work with your
+		#personalized reddit account to deliver the top post that is unique to your account
 		#r.login()
 
+		#using a for loop to loop through the submissions and get all of the propper data. In order to be displayed properly
+		#they have to be concatinated strings, otherwise JSON views them as JSON objects and attempts to display them as so.
 		for submissions in submissions:
 			#print submissions.score, submissions.title, submissions.url, submissions.permalink
-			#redditLinks = [submissions.score, submissions.title, submissions.url, submissions.permalink]
 			redditLinks = "Score: " + str(submissions.score) + ". " + str(submissions.title) + " " + str(submissions.url) + " " + str(submissions.permalink)
 
 		return redditLinks
 
-redditParser().main()
--- server.py
+++ server.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python
 import web
-#import json
-#import re
 from script import redditParser
 
+#the render variable is referenced within index so that when te app runs it renders the appropriate information
 render = web.template.render('')
 
+#this is just to define what the url ending in / is assigned to, in this case it is index
 urls = (
 	'/', 'index'
 	)
 
+#by importing redditParse from the script file, I am able to gather the appropriate information and use it within the index class
 class index:
 
+	#its defined as GET because it is getting the appropriate information
 	def GET(self):
 
 		redditLinks= redditParser().main()
 
+		#returning and instance of index under render
 		return render.index(redditLinks)
 
-class links:
 
-	def GET(self):
-		
-		redditLinks= redditParser().main()
-
-
-index().GET()
-
+#finally the necessary part of the code that once everything is filled out will run it and send the imformation to index.html
 if __name__ == "__main__": 
     app = web.application(urls, globals())
     app.run()   
